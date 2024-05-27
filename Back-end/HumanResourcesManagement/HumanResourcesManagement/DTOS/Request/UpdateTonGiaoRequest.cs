5f632a061f83a644dfab65a1980bab5854a2e3da
tran vinh <vinhtdhe163946@fpt.edu.vn>
2024-05-27 17:55:32 +0700

    Dev_Create_CRUD_DanhMucTonGiao

---
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/DTOS/Request/UpdateTonGiaoRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HumanResourcesManagement.DTOS.Request
+{
+    public class UpdateTonGiaoRequest
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ten khong duoc de trong")]
+        public string Ten { get; set; }
+    }
+}
