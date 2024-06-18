b600284d1b3b6b2cee2a9a1a42ec0be0d5064616
duynshe130132 <duynshe130132@fpt.edu.vn>
2024-06-19 06:20:11 +0700

    Update list.js

--- Front-end/assets/scripts/pages/staff/list.js
+++ Front-end/assets/scripts/pages/staff/list.js
@@ -41,7 +41,7 @@ var TableColumns = [
 
   function backToList(id) {
 
-    window.location.replace(`/pages/staff/resume.html?id=${id}`);
+    window.location.replace(`/pages/staff/resume.html`);
 }
 
 
