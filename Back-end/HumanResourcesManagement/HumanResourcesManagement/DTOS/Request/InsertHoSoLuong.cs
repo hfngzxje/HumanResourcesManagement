0c15fd346aa79bcfb07f1c3ce1ae438ddfe96b14
hfngzxje <hungbdhe163402@fpt.edu.vn>
2024-07-27 22:32:52 +0700

    Dev_be__update_api_HoSoLuong

--- Back-end/HumanResourcesManagement/HumanResourcesManagement/DTOS/Request/InsertHoSoLuong.cs
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/DTOS/Request/InsertHoSoLuong.cs
@@ -6,17 +6,16 @@ namespace HumanResourcesManagement.DTOS.Request
     {
         [Required(ErrorMessage = "Ma hop dong khong duoc de trong")]
         public string? Mahopdong { get; set; }
-        public int? Nhomluong { get; set; }
-        [Required(ErrorMessage = "He so luong khong duoc de trong")]
-        public double? Hesoluong { get; set; }
-        public string? Bacluong { get; set; }
         public double? Phucaptrachnhiem { get; set; }
         public double? Phucapkhac { get; set; }
+        public float? TongLuong { get; set; }
         public string? Thoihanlenluong { get; set; }
         [Required(ErrorMessage = "Ngay hieu luc khong duoc de trong")]
         public DateTime? Ngayhieuluc { get; set; }
         [Required(ErrorMessage = "Ngay ket thuc khong duoc de trong")]
         public DateTime? Ngayketthuc { get; set; }
         public string? Ghichu { get; set; }
+
+        public int? Nhomluong { get; set; }
     }
 }
