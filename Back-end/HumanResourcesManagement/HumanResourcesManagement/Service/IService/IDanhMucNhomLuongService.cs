b9004d8cf97281c39e4e20fd8dc2c9ca1a71ad77
hfngzxje <hungbdhe163402@fpt.edu.vn>
2024-07-27 22:23:43 +0700

    Dev_Be_Api_CRUD_NhomLuong

--- Back-end/HumanResourcesManagement/HumanResourcesManagement/Service/IService/IDanhMucNhomLuongService.cs
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/Service/IService/IDanhMucNhomLuongService.cs
@@ -1,6 +1,19 @@
-﻿namespace HumanResourcesManagement.Service.IService
+﻿using HumanResourcesManagement.DTOS.Request;
+using HumanResourcesManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResourcesManagement.Service.IService
 {
-    public class IDanhMucNhomLuongService
+    public interface IDanhMucNhomLuongService
     {
+        Task<TblDanhMucNhomLuong> AddNhomLuong(DanhMucNhomLuongRequest request);
+
+        Task<TblDanhMucNhomLuong> UpdateNhomLuongAsync(int id, DanhMucNhomLuongRequest request);
+
+        Task<bool> DeleteNhomLuongAsync(int id);
+
+        Task<List<TblDanhMucNhomLuong>> GetAllNhomLuongAsync();
+
+        Task<TblDanhMucNhomLuong?> GetNhomLuongByIdAsync(int id);
     }
 }
