6b85cfbbfab06458b6dad98f2646e52c047b872a
hoangviet982002 <vietbhhe163168@fpt.edu.vn>
2024-07-28 14:28:52 +0700

    Dev_FixDb
    Dev_FixDb

---
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/Models/EmailHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResourcesManagement.Models
+{
+    public partial class EmailHistory
+    {
+        public int Id { get; set; }
+        public string Ma { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime SentDateTime { get; set; }
+        public string Greeting { get; set; } = null!;
+
+        public virtual TblNhanVien MaNavigation { get; set; } = null!;
+    }
+}
