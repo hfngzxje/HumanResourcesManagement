563bcd185ae0caea0558e1de56f285fe4511cd53
hfngzxje <hungbdhe163402@fpt.edu.vn>
2024-07-28 16:03:24 +0700

    Dev_BE_Create_api_send_email_chucmungsinhnhat

---
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/Config/BirthdayCheckHostedService.cs
@@ -0,0 +1,50 @@
+﻿using HumanResourcesManagement.Service;
+
+namespace HumanResourcesManagement.Config
+{
+    public class BirthdayCheckHostedService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private readonly BirthdayService _birthdayService;
+
+        public BirthdayCheckHostedService(BirthdayService birthdayService)
+        {
+            _birthdayService = birthdayService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(CheckBirthdays, null, GetInitialDelay(), TimeSpan.FromDays(1));
+            return Task.CompletedTask;
+        }
+
+        private void CheckBirthdays(object state)
+        {
+            _birthdayService.CheckAndSendBirthdayEmails();
+        }
+
+        private TimeSpan GetInitialDelay()
+        {
+            var now = DateTime.Now;
+            var nextRunTime = new DateTime(now.Year, now.Month, now.Day, 8, 0, 0, 0);
+
+            if (now > nextRunTime)
+            {
+                nextRunTime = nextRunTime.AddDays(1);
+            }
+
+            return nextRunTime - now;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
