8ced2165d0d9d23741e303aafc6a70648f6c474c
hfngzxje <hungbdhe163402@fpt.edu.vn>
2024-08-04 17:13:01 +0700

    Dev_fix_hopdong

--- Back-end/HumanResourcesManagement/HumanResourcesManagement/DTOS/Request/InsertHoSoLuong.cs
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/DTOS/Request/InsertHoSoLuong.cs
@@ -10,10 +10,6 @@ namespace HumanResourcesManagement.DTOS.Request
         public double? Phucapkhac { get; set; }
         public float? Tongluong { get; set; }
         public string? Thoihanlenluong { get; set; }
-        [Required(ErrorMessage = "Ngay hieu luc khong duoc de trong")]
-        public DateTime? Ngayhieuluc { get; set; }
-        [Required(ErrorMessage = "Ngay ket thuc khong duoc de trong")]
-        public DateTime? Ngayketthuc { get; set; }
         public string? Ghichu { get; set; }
 
         public int? Nhomluong { get; set; }
