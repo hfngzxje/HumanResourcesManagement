d237cc153e6d0d8e9546351cd647fca71f85c0a8
duynshe130132 <duynshe130132@fpt.edu.vn>
2024-08-10 00:22:53 +0700

    Update Rp Dang Vien _ Rp Nhom Luong

--- Front-end/assets/scripts/pages/staffSideBar/reportCommunistParty.js
+++ Front-end/assets/scripts/pages/staffSideBar/reportCommunistParty.js
@@ -11,7 +11,7 @@ var TableColumns = [
   {
     label: "Ngày sinh",
     key: "ngaysinh"
-    },
+  },
   {
     label: "Giới tính",
     key: "gioitinh",
@@ -23,13 +23,11 @@ var TableColumns = [
   },
   {
     label: "Ngày vào đảng",
-    key: "ngayVaoDang",
-    // type:'datetime'
+    key: "ngayVaoDang"
   },
   {
     label: "Ngày vào đảng chính thức",
-    key: "ngayVaoDangChinhThuc",
-    // type:'datetime'
+    key: "ngayVaoDangChinhThuc"
   },
   {
     label: "Phòng ban",
@@ -48,63 +46,6 @@ var TableColumns = [
     key: "tamTru",
   },
 ];
-var locTheo = [
-  { label: 'Tất cả', value: 'Tất cả' },
-  { label: 'Quê quán', value: 'Quê quán' },
-  { label: "Phòng ban", value: 'Phòng ban' },
-  { label: "Ngày tháng", value: 'Ngày tháng' },
-  { label: "Giới tính", value: 'Giới tính' },
-  { label: "Năm tuổi đảng", value: 'Năm tuổi đảng' },
-];
-var QueQuan = [
-  { label: "Quê quán", value: "Quê quán" },
-  { label: "Thường trú", value: "Thường trú" },
-  { label: "Tạm trú", value: "Tạm trú" },
-];
-var gioiTinh = [
-  { label: "Tất cả", value: 'Tất cả' },
-  { label: "Nam", value: 'true' },
-  { label: "Nữ", value: 'false' },
-];
-
-document.addEventListener("DOMContentLoaded", () => {
-  const filterSelect = document.querySelector(
-    'base-select[description="Lọc theo"]'
-  );
-  const fromDate = document.querySelector(
-    'base-datepicker[description="Từ ngày"]'
-  );
-  const toDate = document.querySelector(
-    'base-datepicker[description="Đến ngày"]'
-  );
-  const queQuanSelect = document.querySelector(
-    'base-select[description="Địa chỉ"]'
-  );
-  const queQuan = document.querySelector(
-    'base-input[description="Quê quán"]'
-  );
-  const departmentSelect = document.querySelector(
-    'base-select[description="Phòng ban"]'
-  );
-  const genderSelect = document.querySelector(
-    'base-select[description="Giới tính"]'
-  );
-  const namerTuoiDangInput = document.querySelector(
-    'base-input-number[description="Năm tuổi đảng"]'
-  );
-
-  // Hàm để bật hoặc tắt trạng thái của các thẻ input và select
-  function toggleInputs(enabled) {
-    queQuanSelect.disabled = !enabled
-    fromDate.disabled = !enabled;
-    toDate.disabled = !enabled;
-    queQuan.disabled = !enabled;
-    departmentSelect.disabled = !enabled;
-    genderSelect.disabled = !enabled;
-    namerTuoiDangInput.disabled = !enabled;
-  }
-  toggleInputs(filterSelect.value === "Tất cả");
-});
 
 function renderActionByStatus() {
   const actionEl = document.getElementById("report_form_action");
@@ -115,13 +56,8 @@ function renderActionByStatus() {
     btnEl.setAttribute("icon", icon);
     return btnEl;
   };
-  const DisplayBtn = buildButton("Tìm báo cáo", "green", "bx bx-search");
   const pdfBtn = buildButton("PDF", "red", "bx bx-file-blank");
   const excelBtn = buildButton("Excel", "", "bx bx-spreadsheet");
-
-  DisplayBtn.addEventListener("click", () => {
-    handleSearch();
-  });
 
   excelBtn.addEventListener("click", () => {
     handleExportExcel();
@@ -131,11 +67,8 @@ function renderActionByStatus() {
   //   handleExportPDF();
   // });
 
-  actionEl.append(DisplayBtn, pdfBtn, excelBtn);
+  actionEl.append(pdfBtn, excelBtn);
 
-  document.addEventListener("DOMContentLoaded", () => {
-    DisplayBtn.click();
-  });
 }
 
 // _____________________________________excel_________________________________________________________
@@ -149,7 +82,7 @@ async function handleExportExcel() {
   params.append('PhongBan', formValue.PhongBan || '');
   params.append('QueQuan', formValue.QueQuan || '');
   params.append('NamTuoiDang', formValue.NamTuoiDang || '');
-  
+
 
   try {
     const response = await fetch('https://localhost:7141/api/BaoCao/ExportBaoCaoDangVienToExecl', {
@@ -175,7 +108,7 @@ function createDownloadLinkExcel(blob) {
   const url = window.URL.createObjectURL(blob);
   const link = document.createElement('a');
   link.href = url;
-  link.download = 'BaoCao_DanhSachDangVien.xlsx'; 
+  link.download = 'BaoCao_DanhSachDangVien.xlsx';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
@@ -219,7 +152,7 @@ function createDownloadLinkPDF(blob) {
   const url = window.URL.createObjectURL(blob);
   const link = document.createElement('a');
   link.href = url;
-  link.download = 'BaoCao_DanhSachNhanVien.pdf'; 
+  link.download = 'BaoCao_DanhSachNhanVien.pdf';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
@@ -228,111 +161,87 @@ function createDownloadLinkPDF(blob) {
 // _______________________________________________________________________________________________________
 
 
-function handleSearch() {
-  const formValue = getFormValues("report_form");
-  console.log("Form: " , formValue)
-  const tableReport = document.getElementById("tableReport");
-  if (formValue.searchRules === "Tất cả") {
-    tableReport.handleCallFetchData(formValue);
-    return;
+async function handleSearch() {
+  try {
+    const formValue = getFormValues("report_form");
+    const tableReport = document.getElementById("tableReport");
+    const params = {
+      ToDate: formValue.ToDate,
+      FromDate: formValue.FromDate,
+      PhongBan: formValue.PhongBan || "",
+      QueQuan: formValue.QueQuan,
+      NamTuoiDang: formValue.NamTuoiDang,
+      NamVaoDang: formValue.NamVaoDang
+    };
+    await tableReport.handleCallFetchData(params);
   }
-  const params = {
-    searchRules: formValue.searchRules,
-    GioiTinh: "Tất cả",
-    ToDate: "",
-    FromDate:"",
-    PhongBan: "",
-    DiaChi:"",
-    QueQuan:"",
-    NamTuoiDang:"",
-  };
-  if (formValue.searchRules === "Quê quán") {
-    params.searchRules = formValue.DiaChi
-    params.QueQuan = formValue.QueQuan;
-  }
-  if (formValue.searchRules === "Phòng ban") {
-    params.PhongBan = formValue.PhongBan;
-  }
-  if (formValue.searchRules === "Ngày tháng") {
-    params.FromDate = formValue.FromDate;
-    params.ToDate = formValue.ToDate;
-  }
-  if (formValue.searchRules === "Giới tính") {
-    params.GioiTinh = formValue.GioiTinh;
-  }
-  if(formValue.searchRules === "Năm tuổi đảng"){
-    params.NamTuoiDang = formValue.NamTuoiDang
+  catch (error) {
+    console.error("Error in handleSearch:", error);
   }
 
+}
+function queQuanChange() {
+  const phongban = document.querySelector('#quequan input')
+  phongban.addEventListener("change", (event) => {
+      handleSearch()
+  });
+}
+function phongBanChange() {
+  const phongban = document.querySelector('#phongban select')
+  phongban.addEventListener("change", (event) => {
+      handleSearch()
+  });
+}
+function namTuoiDangChange() {
+  const phongban = document.querySelector('#namtuoidang input')
+  phongban.addEventListener("change", (event) => {
+      handleSearch()
+  });
+}
+function namVaoDangChange() {
+  const phongban = document.querySelector('#namvaodang input')
+  phongban.addEventListener("change", (event) => {
+      handleSearch()
+  });
+}
 
-  tableReport.handleCallFetchData(params);
+let fromDateChanged = false;
+let toDateChanged = false;
 
-  console.log("params: ", params)
+function fromChange() {
+  const fromDatePicker = document.querySelector('#tungay input');
+  fromDatePicker.addEventListener("change", (event) => {
+    fromDateChanged = true;
+    dateChange();
+  });
+}
+
+function toChange() {
+  const toDatePicker = document.querySelector('#denngay input');
+  toDatePicker.addEventListener("change", (event) => {
+    toDateChanged = true;
+    dateChange();
+  });
+}
+function dateChange(){
+  if(fromDateChanged && toDateChanged){
+    handleSearch();
+  }
+}
+function inits(){
+  queQuanChange()
+  phongBanChange()
+  namTuoiDangChange()
+  namVaoDangChange()
+  toChange()
+  dateChange()
 }
 
 function buildApiUrl() {
   return apiTable;
 }
-
-function handleSelectFilterBy() {
-  const locTheoEl = document.querySelector("#loctheo select");
-  const DiaChiEl = document.querySelector("#selectquequan select");
-  const queQuanEl = document.querySelector("#quequan input");
-  const phongBanEl = document.querySelector("#phongban select");
-  const tuNgayEl = document.querySelector("#tungay input");
-  const denNgayEl = document.querySelector("#denngay input");
-  const gioiTinhEl = document.querySelector("#gioitinh select");
-  const namTuoiDangEl = document.querySelector("#namtuoidang input");
-  locTheoEl.addEventListener("input", () => {
-    const locTheoValue = locTheoEl.value;
-    console.log("locTheoValue ", locTheoValue);
-
-    if (locTheoValue === "Tất cả") {
-      DiaChiEl.disabled = false;
-      queQuanEl.disabled = false;
-      phongBanEl.disabled = false;
-      tuNgayEl.disabled = false;
-      denNgayEl.disabled = false;
-      gioiTinhEl.disabled = false;
-      namTuoiDangEl.disabled = false;
-      return;
-    }
-    DiaChiEl.disabled = true;
-    queQuanEl.disabled = true;
-    phongBanEl.disabled = true;
-    tuNgayEl.disabled = true;
-    denNgayEl.disabled = true;
-    gioiTinhEl.disabled = true;
-    namTuoiDangEl.disabled = true;
-    if (locTheoValue === "Quê quán") {
-      DiaChiEl.disabled = false;
-      queQuanEl.disabled = false;
-      phongBanEl.value ="";
-    }
-    if (locTheoValue === "Phòng ban") {
-      phongBanEl.disabled = false;
-      gioiTinhEl.value= "Tất cả"
-    }
-    if (locTheoValue === "Ngày tháng") {
-      tuNgayEl.disabled = false;
-      denNgayEl.disabled = false;
-      phongBanEl.value ="";
-      gioiTinhEl.value= "Tất cả"
-    }
-    if (locTheoValue === "Giới tính") {
-      gioiTinhEl.disabled = false;
-      phongBanEl.value ="";
-    }
-    if (locTheoValue === "Năm tuổi đảng") {
-        namTuoiDangEl.disabled = false;
-        phongBanEl.value ="";
-      }
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.log(123);
   renderActionByStatus();
-  handleSelectFilterBy();
   handleSearch();
+  inits()
 });
