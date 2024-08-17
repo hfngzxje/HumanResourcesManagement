20ce7ee5c342303d670db5fe5e159cf24ade33cb
tran vinh <vinhtdhe163946@fpt.edu.vn>
2024-08-18 01:48:19 +0700

    Dev_Hotfix

--- Back-end/HumanResourcesManagement/HumanResourcesManagement/Service/IService/IDanhSachLenLuongService.cs
+++ Back-end/HumanResourcesManagement/HumanResourcesManagement/Service/IService/IDanhSachLenLuongService.cs
@@ -17,13 +17,13 @@ namespace HumanResourcesManagement.Service.IService
         Task<(byte[] fileContent, string fileName)> ExportLenLuongToExcel();
         Task<(byte[] fileContent, string fileName)> ExportLenLuongToPdf();
 
-        Task<IEnumerable<DanhSachNangLuongResponse>> GetAllAsync(int? phongId, int? toId, string? maNV);
+        Task<IEnumerable<DanhSachNangLuongResponse>> GetAllAsync(int? phongId, int? chucDanhId);
 
         Task<DanhSachNangLuongDetailsResponse?> GetByIdAsync(int id);
 
         Task<bool> DeleteAsync(int id);
 
-        Task<IEnumerable<DanhSachNangLuongResponse>> GetAllStatus1And3Async(int? phongId, int? toId, string? maNV);
+        Task<IEnumerable<DanhSachNangLuongResponse>> GetAllStatus1And3Async(int? phongId, int? chucDanhId);
         Task<IEnumerable<DanhSachNangLuongResponse>> GetAllStatus2Async();
 
     }
