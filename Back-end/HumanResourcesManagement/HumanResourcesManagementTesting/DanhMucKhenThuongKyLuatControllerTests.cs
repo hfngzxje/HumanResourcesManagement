854f4e4d8966428b9e94bb5d35c7ae07ce3eb24f
Nguyễn Tuấn Dũng <97139064+dungnthe150915FPT@users.noreply.github.com>
2024-08-27 22:27:43 +0700

    Tester_UnitTest_Back-End
    Tester_UnitTest_Back-End

--- Back-end/HumanResourcesManagement/HumanResourcesManagementTesting/DanhMucKhenThuongKyLuatControllerTests.cs
+++ Back-end/HumanResourcesManagement/HumanResourcesManagementTesting/DanhMucKhenThuongKyLuatControllerTests.cs
@@ -10,156 +10,105 @@ using HumanResourcesManagement.DTOS.Response;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using HumanResourcesManagement.Service;
+using HumanResourcesManagement.Config.Mapper;
 
 namespace HumanResourcesManagement.Tests.Controllers
 {
     [TestFixture]
     public class DanhMucKhenThuongKyLuatControllerTests
     {
-        private Mock<IDanhMucKhenThuongKyLuatService> _danhMucKhenThuongKyLuatServiceMock;
-        private Mock<NhanSuContext> _contextMock;
-        private DanhMucKhenThuongKyLuatController _controller;
-        private List<DanhMucKhenThuongKyLuatResponse> _danhMucKhenThuongKyLuats;
+        private NhanSuContext _context;
+        private IDanhMucKhenThuongKyLuatService _danhMucMucKhenThuongKyLuatService;
+        private DanhMucKhenThuongKyLuatController _danhMucKhenThuongKyLuatController;
 
         [SetUp]
         public void Setup()
         {
-            _danhMucKhenThuongKyLuatServiceMock = new Mock<IDanhMucKhenThuongKyLuatService>();
-            _contextMock = new Mock<NhanSuContext>(new DbContextOptions<NhanSuContext>());
-            _controller = new DanhMucKhenThuongKyLuatController(_danhMucKhenThuongKyLuatServiceMock.Object, _contextMock.Object);
+            var options = new DbContextOptionsBuilder<NhanSuContext>()
+                .UseInMemoryDatabase(databaseName: "NhanSu")
+                .Options;
 
-            // Khai báo danh sách khen thưởng kỷ luật
-            _danhMucKhenThuongKyLuats = new List<DanhMucKhenThuongKyLuatResponse>
-            {
-                new DanhMucKhenThuongKyLuatResponse { Id = 1, Ten = "Khen thưởng A", Ma = "KA1" },
-                new DanhMucKhenThuongKyLuatResponse { Id = 2, Ten = "Khen thưởng B", Ma = "KB2" },
-                new DanhMucKhenThuongKyLuatResponse { Id = 3, Ten = "Khen thưởng C", Ma = "KC3" }
-            };
+            _context = new NhanSuContext(options);
 
-            // Setup mock service trả về danh sách
-            _danhMucKhenThuongKyLuatServiceMock.Setup(s => s.GetDanhMucKhenThuongKyLuat()).ReturnsAsync(_danhMucKhenThuongKyLuats);
+            _context.TblDanhMucKhenThuongKyLuats.AddRange(
+                new TblDanhMucKhenThuongKyLuat { Id = 1, Ma = "KT1", Ten = "Nhân viên xuất sắc" },
+                new TblDanhMucKhenThuongKyLuat { Id = 2, Ma = "KT1", Ten = "Đi làm muộn" }
+            );
+            _context.SaveChanges();
+
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile<NhanVienMapper>());
+            var mapper = mapperConfig.CreateMapper();
+            _danhMucMucKhenThuongKyLuatService = new DanhMucKhenThuongKyLuatService(mapper, _context);
+            _danhMucKhenThuongKyLuatController = new DanhMucKhenThuongKyLuatController(_danhMucMucKhenThuongKyLuatService, _context);
         }
 
         [Test]
-        public async Task GetDanhMucKhenThuongKyLuat_ReturnsOkResult_WithListOfDanhMucKhenThuongKyLuat()
+        public async Task GetDanhMucKhenThuongKyLuat_ShouldReturnAllKhenThuongKyLuats()
         {
-            // Act
-            var result = await _controller.GetDanhMucKhenThuongKyLuat();
+            var result = await _danhMucKhenThuongKyLuatController.GetDanhMucKhenThuongKyLuat();
 
-            // Assert
             Assert.IsInstanceOf<OkObjectResult>(result);
             var okResult = result as OkObjectResult;
-            Assert.AreEqual(_danhMucKhenThuongKyLuats, okResult.Value);
-
-            // In danh sách khen thưởng kỷ luật
-            Console.WriteLine("Danh sách khen thưởng kỷ luật:");
-            foreach (var item in _danhMucKhenThuongKyLuats)
-            {
-                Console.WriteLine($"Id: {item.Id}, Tên: {item.Ten}, Mã: {item.Ma}");
-            }
+            var khenThuongKyLuats = okResult?.Value as IEnumerable<DanhMucKhenThuongKyLuatResponse>;
+            Assert.AreEqual(2, khenThuongKyLuats?.Count());
         }
 
         [Test]
-        public async Task GetDanhMucKhenThuongKyLuatById_ReturnsOkResult_WithDanhMucKhenThuongKyLuat()
+        public async Task GetDanhMucKhenThuongKyLuatById_ShouldReturnCorrectKhenThuongKyLuat()
         {
-            // Arrange
-            var danhMucKhenThuongKyLuat = _danhMucKhenThuongKyLuats.FirstOrDefault(d => d.Id == 1);
-            _danhMucKhenThuongKyLuatServiceMock.Setup(s => s.GetDanhMucKhenThuongKyLuatById(1)).ReturnsAsync(danhMucKhenThuongKyLuat);
-
-            // Act
-            var result = await _controller.GetDanhMucKhenThuongKyLuatById(1);
-
-            // Assert
+            var result = await _danhMucKhenThuongKyLuatController.GetDanhMucKhenThuongKyLuatById(1);
             Assert.IsInstanceOf<OkObjectResult>(result);
             var okResult = result as OkObjectResult;
-            Assert.AreEqual(danhMucKhenThuongKyLuat, okResult.Value);
-
-            // In thông tin khen thưởng kỷ luật với Id tương ứng
-            Console.WriteLine($"Khen thưởng kỷ luật Id: {danhMucKhenThuongKyLuat.Id}, Tên: {danhMucKhenThuongKyLuat.Ten}, Mã: {danhMucKhenThuongKyLuat.Ma}");
+            var khenThuongKyLuat = okResult?.Value as DanhMucKhenThuongKyLuatResponse;
+            Assert.AreEqual("KT1", khenThuongKyLuat?.Ma);
+            Assert.AreEqual("Nhân viên xuất sắc", khenThuongKyLuat?.Ten);
         }
 
         [Test]
-        public async Task AddDanhMucKhenThuongKyLuat_ReturnsStatusCode200_WhenAdditionIsSuccessful()
+        public async Task AddDanhMucKhenThuongKyLuat_ShouldAddNewKhenThuongKyLuat()
         {
-            // Arrange
-            var req = new DanhMucKhenThuongKyLuatRequest { Ten = "Khen thưởng D" };
+            var newKhenThuongKyLuat = new DanhMucKhenThuongKyLuatRequest { Ten = "Nhân viên giỏi" };
 
-            // Act
-            var result = await _controller.AddDanhMucKhenThuongKyLuat(req);
+            var result = await _danhMucKhenThuongKyLuatController.AddDanhMucKhenThuongKyLuat(newKhenThuongKyLuat);
 
-            // Assert
-            Assert.IsInstanceOf<StatusCodeResult>(result);
-            var statusCodeResult = result as StatusCodeResult;
-            Assert.AreEqual(200, statusCodeResult.StatusCode);
+            Assert.IsInstanceOf<ObjectResult>(result);
+            var okResult = result as ObjectResult;
+            Assert.AreEqual(200, okResult?.StatusCode);
 
-            // Thêm khen thưởng kỷ luật mới vào danh sách
-            var newId = _danhMucKhenThuongKyLuats.Max(d => d.Id) + 1;
-            var newDanMucKhenThuongKyLuat = new DanhMucKhenThuongKyLuatResponse { Id = newId, Ten = req.Ten, Ma = "KD" + newId };
-            _danhMucKhenThuongKyLuats.Add(newDanMucKhenThuongKyLuat);
-
-            // In danh sách khen thưởng kỷ luật sau khi thêm mới
-            Console.WriteLine("Danh sách khen thưởng kỷ luật sau khi thêm mới:");
-            foreach (var item in _danhMucKhenThuongKyLuats)
-            {
-                Console.WriteLine($"Id: {item.Id}, Tên: {item.Ten}, Mã: {item.Ma}");
-            }
+            var khenThuongKyLuats = await _context.TblDanhMucKhenThuongKyLuats.ToListAsync();
+            Assert.AreEqual(3, khenThuongKyLuats.Count);
+            Assert.IsTrue(khenThuongKyLuats.Any(ng => ng.Ten == "Nhân viên giỏi"));
         }
 
         [Test]
-        public async Task UpdateDanhMucKhenThuongKyLuat_ReturnsStatusCode200_WhenUpdateIsSuccessful()
+        public async Task DeleteDanhMucKhenThuongKyLuat_ShouldRemoveKhenThuongKyLuat()
         {
-            int id = 1;
-            // Arrange
-            var req = new DanhMucKhenThuongKyLuatRequest { Ten = "Khen thưởng E" };
+            var result = await _danhMucKhenThuongKyLuatController.DeleteDanhMucKhenThuongKyLuat(1);
 
-            // Act
-            var result = await _controller.UpdateDanhMucKhenThuongKyLuat(req, id);
+            Assert.IsInstanceOf<ObjectResult>(result);
+            var okResult = result as ObjectResult;
+            Assert.AreEqual(200, okResult?.StatusCode);
 
-            // Assert
-            Assert.IsInstanceOf<StatusCodeResult>(result);
-            var statusCodeResult = result as StatusCodeResult;
-            Assert.AreEqual(200, statusCodeResult.StatusCode);
-
-            // Cập nhật khen thưởng kỷ luật trong danh sách
-            var updatedDanMucKhenThuongKyLuat = _danhMucKhenThuongKyLuats.FirstOrDefault(d => d.Id == id);
-            if (updatedDanMucKhenThuongKyLuat != null)
-            {
-                updatedDanMucKhenThuongKyLuat.Ten = req.Ten;
-                updatedDanMucKhenThuongKyLuat.Ma = "KE" + id;
-            }
-
-            // In danh sách khen thưởng kỷ luật sau khi cập nhật
-            Console.WriteLine("Danh sách khen thưởng kỷ luật sau khi cập nhật:");
-            foreach (var item in _danhMucKhenThuongKyLuats)
-            {
-                Console.WriteLine($"Id: {item.Id}, Tên: {item.Ten}, Mã: {item.Ma}");
-            }
+            var khenThuongKyLuats = await _context.TblDanhMucKhenThuongKyLuats.ToListAsync();
+            Assert.AreEqual(1, khenThuongKyLuats.Count);
+            Assert.IsFalse(khenThuongKyLuats.Any(ng => ng.Id == 1));
         }
 
         [Test]
-        public async Task DeleteDanhMucKhenThuongKyLuat_ReturnsStatusCode200_WhenDeletionIsSuccessful()
+        public async Task UpdateDanhMucKhenThuongKyLuat_ShouldUpdateExistingKhenThuongKyLuat()
         {
-            // Arrange
-            int id = 2;
+            var updateRequest = new DanhMucKhenThuongKyLuatRequest { Ten = "Nhân viên giỏi" };
 
-            // Act
-            var result = await _controller.DeleteDanhMucKhenThuongKyLuat(id);
+            var result = await _danhMucKhenThuongKyLuatController.UpdateDanhMucKhenThuongKyLuat(updateRequest, 2);
 
-            // Assert
-            Assert.IsInstanceOf<StatusCodeResult>(result);
-            var statusCodeResult = result as StatusCodeResult;
-            Assert.AreEqual(200, statusCodeResult.StatusCode);
+            Assert.IsInstanceOf<ObjectResult>(result);
+            var okResult = result as ObjectResult;
+            Assert.AreEqual(200, okResult?.StatusCode);
 
-            // Xóa khen thưởng kỷ luật có Id = 2 khỏi danh sách
-            var deletedDanMucKhenThuongKyLuat = _danhMucKhenThuongKyLuats.FirstOrDefault(d => d.Id == id);
-            _danhMucKhenThuongKyLuats.Remove(deletedDanMucKhenThuongKyLuat);
-
-            // In danh sách khen thưởng kỷ luật sau khi xóa
-            Console.WriteLine("Danh sách khen thưởng kỷ luật sau khi xóa:");
-            foreach (var item in _danhMucKhenThuongKyLuats)
-            {
-                Console.WriteLine($"Id: {item.Id}, Tên: {item.Ten}, Mã: {item.Ma}");
-            }
+            var updatedKhenThuongKyLuat = await _context.TblDanhMucKhenThuongKyLuats.FindAsync(2);
+            Assert.AreEqual("Nhân viên giỏi", updatedKhenThuongKyLuat.Ten);
         }
     }
 }
